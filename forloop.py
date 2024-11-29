19d8a92bcc3c2ca0e60f98056549df3f8cc97fb7
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-11-29 14:23:13 +0530

    exe

--- forloop.py
+++ forloop.py
@@ -19,6 +19,40 @@ for x in "share":  #s
                    #a
                    #r
                    #e
+for x in range(10): # 0 1 2 3 4 5 6 7 8 9
+    print(x)
+
+for x in range(2,4):
+    print(x)          #2 3
+
+for x in range(0,101,5):
+    print(x)  #o/p will be 0 5 10 15 20 25 30 35 40......
+
+print('')
+
+names = ["vino","abi","hari"]
+actions = ["codes","eats","sleeps"]
+
+for name in names: 
+                    
+ for action in actions:
+        print(name + " " + action + ".")   #vino codes.
+                                           # vino eats.
+                                           #vino sleeps.
+                                           #abi codes.
+                                           #abi eats.
+                                           #abi sleeps.
+                                           #hari codes.
+                                           #hari eats.
+                                           #hari sleeps.
+for name in names: 
+                    
+ for action in actions:
+        print(name + " " + action + ".") 
+
+
+
+
 
 
 
---
+++ functions.py
@@ -0,0 +1,20 @@
+#functions
+
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+print('')
+def sum(num1 ,num2):
+    return num1 + num2
+
+total = sum(70,20)
+print(total)
+
+print('')
+
+
+
+
+
